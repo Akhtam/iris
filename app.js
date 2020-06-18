8f72f5f2aa5d1c766f3955ee08edd663c8878267
akhtam <akhtam.ismatov@gmail.com>
2020-06-18 15:22:54 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
