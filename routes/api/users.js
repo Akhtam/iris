e4579be981dadba6b6cfdf09eb4fae2a3cce3a1f
akhtam <akhtam.ismatov@gmail.com>
2020-06-25 00:30:12 -0700

    handle user auth on backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const secretOrKey = require('../../config/keys').secretOrKey;
 const User = require('../../models/User');
+const validateUserAuth = require('../../validation/authValidation');
 
 //Register User
 router.post('/register', async (req, res) => {
   const { username, password } = req.body;
+
+  const { errors, isValid } = validateUserAuth(username, password, 'register');
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const user = await User.findOne({ username });
   if (user) return res.status(400).json({ msg: 'Username already taken!' });
   const newUser = new User({ username, password });
+
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(newUser.password, salt, async (err, hash) => {
       if (err) throw err;
       try {
         newUser.password = hash;
         const hashedUser = await newUser.save();
-        return res.json(hashedUser);
+        const payload = { id: hashedUser._id, username: hashedUser.username };
+        jwt.sign(payload, secretOrKey, { expiresIn: 3600 }, (err, token) => {
+          res.json({ success: true, token: 'Bearer ' + token });
+        });
       } catch (error) {
         console.log(error);
       }
@@ -26,11 +40,28 @@ router.post('/register', async (req, res) => {
 //Login User
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
+
+  const { errors, isValid } = validateUserAuth(username, password, 'login');
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const user = await User.findOne({ username });
-  if (!user) return res.status(400).json({ msg: 'Wrong credentials!' });
+  if (!user) {
+    errors.username = 'Username does not exist';
+    return res.status(400).json(errors);
+  }
+
   const isMatch = await bcrypt.compare(password, user.password);
-  if (isMatch) return res.json({ msg: 'Succes' });
-  return res.status(400).json({ msg: 'Wrong credentials!' });
+  if (isMatch) {
+    const payload = { id: user._id, username: user.username };
+    jwt.sign(payload, secretOrKey, { expiresIn: 3600 }, (err, token) => {
+      res.json({ success: true, token: 'Bearer ' + token });
+    });
+  } else {
+    errors.password = 'Incorrect password';
+    res.status(400).json(errors);
+  }
 });
 
 module.exports = router;
